9e68446a7888c227cc3b8b66f6aba4294034ca17
Jasneet Sabharwal <jasneet.sabharwal@gmail.com>
2016-01-07 21:55:53 -0800

    updated coarsebilm

--- moses/FF/CoarseBiLM.cpp
+++ moses/FF/CoarseBiLM.cpp
@@ -5,76 +5,48 @@
 
 using namespace std;
 
-namespace Moses
-{
-int CoarseBiLMState::Compare(const FFState& other) const
-{
-  const CoarseBiLMState &otherState = static_cast<const CoarseBiLMState&>(other);
+namespace Moses {
+int CoarseBiLMState::Compare(const FFState& other) const {
+	const CoarseBiLMState &otherState =
+			static_cast<const CoarseBiLMState&>(other);
 
-  if (m_targetLen == otherState.m_targetLen)
-    return 0;
-  return (m_targetLen < otherState.m_targetLen) ? -1 : +1;
+	if (m_targetLen == otherState.m_targetLen)
+		return 0;
+	return (m_targetLen < otherState.m_targetLen) ? -1 : +1;
 }
 
 ////////////////////////////////////////////////////////////////
-CoarseBiLM::CoarseBiLM(const std::string &line)
-  :StatefulFeatureFunction(3, line)
-{
-  ReadParameters();
+CoarseBiLM::CoarseBiLM(const std::string &line) :
+		StatefulFeatureFunction(3, line) {
+	ReadParameters();
 }
 
-void CoarseBiLM::EvaluateInIsolation(const Phrase &source
-    , const TargetPhrase &targetPhrase
-    , ScoreComponentCollection &scoreBreakdown
-    , ScoreComponentCollection &estimatedFutureScore) const
-{}
+FFState* CoarseBiLM::EvaluateWhenApplied(const Hypothesis& cur_hypo,
+		const FFState* prev_state,
+		ScoreComponentCollection* accumulator) const {
+	// dense scores
+	vector<float> newScores(m_numScoreComponents);
+	newScores[0] = 1.5;
+	newScores[1] = 0.3;
+	newScores[2] = 0.4;
+	accumulator->PlusEquals(this, newScores);
 
-void CoarseBiLM::EvaluateWithSourceContext(const InputType &input
-    , const InputPath &inputPath
-    , const TargetPhrase &targetPhrase
-    , const StackVec *stackVec
-    , ScoreComponentCollection &scoreBreakdown
-    , ScoreComponentCollection *estimatedFutureScore) const
-{}
+	// sparse scores
+	accumulator->PlusEquals(this, "sparse-name", 2.4);
+	cur_hypo.GetCurrTargetPhrase();
+	string sourcePhraseStringRep = cur_hypo.GetSourcePhraseStringRep();
 
-void CoarseBiLM::EvaluateTranslationOptionListWithSourceContext(const InputType &input
-    , const TranslationOptionList &translationOptionList) const
-{}
-
-FFState* CoarseBiLM::EvaluateWhenApplied(
-  const Hypothesis& cur_hypo,
-  const FFState* prev_state,
-  ScoreComponentCollection* accumulator) const
-{
-  // dense scores
-  vector<float> newScores(m_numScoreComponents);
-  newScores[0] = 1.5;
-  newScores[1] = 0.3;
-  newScores[2] = 0.4;
-  accumulator->PlusEquals(this, newScores);
-
-  // sparse scores
-  accumulator->PlusEquals(this, "sparse-name", 2.4);
-
-  // int targetLen = cur_hypo.GetCurrTargetPhrase().GetSize(); // ??? [UG]
-  return new CoarseBiLMState(0);
+	// int targetLen = cur_hypo.GetCurrTargetPhrase().GetSize(); // ??? [UG]
+	return new CoarseBiLMState(0);
 }
 
-FFState* CoarseBiLM::EvaluateWhenApplied(
-  const ChartHypothesis& /* cur_hypo */,
-  int /* featureID - used to index the state in the previous hypotheses */,
-  ScoreComponentCollection* accumulator) const
-{
-  return new CoarseBiLMState(0);
-}
-
-void CoarseBiLM::SetParameter(const std::string& key, const std::string& value)
-{
-  if (key == "arg") {
-    // set value here
-  } else {
-    StatefulFeatureFunction::SetParameter(key, value);
-  }
+void CoarseBiLM::SetParameter(const std::string& key,
+		const std::string& value) {
+	if (key == "arg") {
+		// set value here
+	} else {
+		StatefulFeatureFunction::SetParameter(key, value);
+	}
 }
 
 }
